9ab2bb8ef4531ef61c89421c9fe49b9c183be0e3
Jacob Samro <jacob@aroliant.com>
2014-12-01 11:08:45 +0530

    shit.js original
    Original File

---
+++ shit.js
@@ -0,0 +1,82 @@
+var shit = {
+	name:'SHiT.JS',
+	version:'0.1',
+	not_found:' Unable to find the element',
+	show 	: function(to){ 
+	if(to.substr(0,1)=='#'){
+to = to.substr(1,to.length);
+		document.getElementById(to).style.display='block'; 
+	}
+	else if(to.substr(0,1)=='.'){
+to = to.substr(1,to.length);
+var hides = document.getElementsByClassName(to);
+for(var i = 0; i < hides.length; i++) {
+         hides[i].style.display = 'block';
+      }
+	}
+	else{
+var hides = document.getElementsByTagName(to);
+if(hides.length!=0){
+for(var i = 0; i < hides.length; i++) {
+         hides[i].style.display = 'block';
+      }
+  }
+  else{
+  		console.log(shit.name + shit.not_found);
+  }
+	}},
+
+	hide 	: function(to){ 
+	if(to.substr(0,1)=='#'){
+to = to.substr(1,to.length);
+		document.getElementById(to).style.display='none'; 
+	}
+	else if(to.substr(0,1)=='.'){
+to = to.substr(1,to.length);
+var hides = document.getElementsByClassName(to);
+for(var i = 0; i < hides.length; i++) {
+         hides[i].style.display = 'none';
+      }
+	}
+	else{
+var hides = document.getElementsByTagName(to);
+if(hides.length!=0){
+for(var i = 0; i < hides.length; i++) {
+         hides[i].style.display = 'none';
+      }
+  }
+  else{
+  		console.log(shit.name + shit.not_found);
+  }
+	}},
+
+	toggle	:function(to){
+	if(to.substr(0,1)=='#'){
+to = to.substr(1,to.length);
+document.getElementById(to).style.display=='block'?document.getElementById(to).style.display='none':document.getElementById(to).style.display='block';
+	}
+	else if(to.substr(0,1)=='.'){
+to = to.substr(1,to.length);
+var hides = document.getElementsByClassName(to);
+for(var i = 0; i < hides.length; i++) {
+hides[i].style.display=='block'?hides[i].style.display='none':hides[i].style.display='block';
+      }
+	}
+	else{
+var hides = document.getElementsByTagName(to);
+if(hides.length!=0){
+for(var i = 0; i < hides.length; i++) {
+hides[i].style.display=='block'?hides[i].style.display='none':hides[i].style.display='block';
+      }
+  }
+  else{
+  	console.log(shit.name + shit.not_found);
+  }
+	}}
+
+};
+
+
+
+
+
