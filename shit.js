cd50cff3276f36a2f75b623f2e079beb41059924
Jacob Samro <jacobsamro.441@gmail.com>
2015-02-02 22:14:04 +0530

    Size reduced, shit.min.js bug fixed

--- shit.js
+++ shit.js
@@ -6,15 +6,14 @@ var shit = {
 if(to.substr(0,1)=='#')
 	{
 		to = to.substr(1,to.length);
-		var e = document.getElementById(to);
+		var e = this.fi(to);
 		e == undefined ? this.log():e.style.display='block'; 
 	}
 	else if(to.substr(0,1)=='.')
 	{
 		to = to.substr(1,to.length);
-		var shows = document.getElementsByClassName(to);
-		if(shows.length==0)
-			this.log()
+		var shows = this.fc(to);
+		if(shows.length==0) {	this.log(); }
 		else{
 			for(var i = 0; i < shows.length; i++)
 				{
@@ -24,9 +23,8 @@ if(to.substr(0,1)=='#')
 	}
 	else
 	{
-		var shows = document.getElementsByTagName(to);
-		if(shows.length==0)
-			this.log();
+		var shows = this.ft(to);
+		if(shows.length==0){	this.log(); }
 		else
 		{
 			for(var i = 0; i < shows.length; i++) 
@@ -41,15 +39,14 @@ if(to.substr(0,1)=='#')
 	if(to.substr(0,1)=='#')
 	{
 		to = to.substr(1,to.length);
-		var e = document.getElementById(to);
+		var e = this.fi(to);
 		e == undefined ? this.log():e.style.display='none'; 
 	}
 	else if(to.substr(0,1)=='.')
 	{
 		to = to.substr(1,to.length);
-		var hides = document.getElementsByClassName(to);
-		if(hides.length==0)
-			this.log()
+		var hides = this.fc(to);
+		if(hides.length==0){	this.log(); }
 		else{
 			for(var i = 0; i < hides.length; i++)
 				{
@@ -59,9 +56,8 @@ if(to.substr(0,1)=='#')
 	}
 	else
 	{
-		var hides = document.getElementsByTagName(to);
-		if(hides.length==0)
-			this.log();
+		var hides = this.ft(to);
+		if(hides.length==0){	this.log(); }
 		else
 		{
 			for(var i = 0; i < hides.length; i++) 
@@ -76,51 +72,41 @@ if(to.substr(0,1)=='#')
 	if(to.substr(0,1)=='#')
 	{
 		to = to.substr(1,to.length);
-		var e = document.getElementById(to);
-		if(e == undefined )
-			this.log()
-		else
-			e.style.display=='block'?e.style.display='none':e.style.display='block'; 
+		var e = this.fi(to);
+		if(e == undefined ){	this.log(); }
+		else{e.style.display=='block'?e.style.display='none':e.style.display='block'; 		}
 	}
 	else if(to.substr(0,1)=='.')
 	{
 		to = to.substr(1,to.length);
-		var togs = document.getElementsByClassName(to);
-		if(togs.length==0)
-			this.log()
+		var togs = this.fc(to);
+		if(togs.length==0){	this.log(); }
 		else{
 			for(var i = 0; i < togs.length; i++)
 				{
-					if(togs[i]==undefined)
-						this.log()
-					else
-						togs[i].style.display == 'none'?togs[i].style.display = 'block':togs[i].style.display = 'none';
+					if(togs[i]==undefined){	this.log(); }
+					else{ togs[i].style.display == 'none'?togs[i].style.display = 'block':togs[i].style.display = 'none';	}
 				}
 			}
 	}
 	else
 	{
-		var togs = document.getElementsByTagName(to);
-		if(togs.length==0)
-			this.log();
+		var togs = this.ft(to);
+		if(togs.length==0){	this.log(); }
 		else
 		{
 			for(var i = 0; i < togs.length; i++) 
 				{
-					if(togs[i]==undefined)
-						this.log()
-					else
-						togs[i].style.display == 'none'?togs[i].style.display = 'block':togs[i].style.display = 'none';
+					if(togs[i]==undefined){	this.log(); }
+					else {	togs[i].style.display == 'none'?togs[i].style.display = 'block':togs[i].style.display = 'none';	}
 				}
 		}
 	}
 },
 	log:function(){
 		console.log(shit.name + shit.not_found);
-	}	
+	},
+	fi : function(_ei){ return document.getElementById(_ei);	},
+	fc : function(_ec){ return document.getElementsByClassName(_ec);	},
+	ft : function(_et){ return document.getElementsByTagName(_et);	}
 };
-
-
-
-
-
